7dd4d21be657776e594370b39228c2bd1714ebd2
Nadav Fogel <nadav.fogel@gmail.com>
2021-05-26 22:23:43 +0300

    Centering of image is working

--- AI/SaveAndResize.py
+++ AI/SaveAndResize.py
@@ -2,6 +2,11 @@ from PIL import Image
 import numpy as np
 import os
 import time
+import imageio as iio
+from skimage import filters
+from skimage.color import rgb2gray  # only needed for incorrectly saved images
+from skimage.measure import regionprops
+import cv2
 
 
 def save_file():
@@ -34,4 +39,36 @@ def save_file():
     image = Image.open('privateImage.png')
     new_image = image.resize((28, 28))
     new_image.save('privateImage.png')
+    center_image()
 
+
+def center_image():
+    # Read image
+    img = cv2.imread("privateImage.png")
+
+    # finding center of mass of image
+    image = rgb2gray(iio.imread("privateImage.png"))
+    threshold_value = filters.threshold_otsu(image)
+    labeled_foreground = (image > threshold_value).astype(int)
+    properties = regionprops(labeled_foreground, image)
+    center_of_mass = properties[0].centroid
+
+    # size of the image
+    num_rows, num_cols = img.shape[:2]
+
+    # finding how much to move in each direction
+    move_x = center_of_mass[1] - num_rows/2
+    move_y = center_of_mass[0] - num_rows/2
+
+    # Creating a translation matrix
+    translation_matrix = np.float32([[1, 0, -move_x], [0, 1, -move_y]])
+
+    # Image translation
+    img_translation = cv2.warpAffine(img, translation_matrix, (num_cols + 0, num_rows + 0))
+
+    # Save image
+    cv2.imwrite("privateImage.png", img_translation)
+
+    # Remove the rbg type of image by converting it to grayscale & saving it
+    new_image = Image.fromarray(np.array(Image.open('privateImage.png').convert('L')))
+    new_image.save('privateImage.png')
