608f2c77e0cabf8e026d98db0bf48330a4ad9261
Nadav Fogel <nadav.fogel@gmail.com>
2021-06-05 23:45:53 +0300

    Merged SaveAndResize & MakePrediction

--- AI/AI.py
+++ AI/AI.py
@@ -1,41 +1,98 @@
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
 from numpy import asarray
+import time
 from PIL import Image
-
-data = pd.read_csv('train.csv')
-
-data = np.array(data)
-amount_of_rows, amount_of_features = data.shape
-np.random.shuffle(data)  # shuffle before splitting into dev and training sets
-
-data_dev = data[0:1000].T
-Y_dev = data_dev[0]
-X_dev = data_dev[1:amount_of_features]
-X_dev = X_dev / 255.
-
-data_train = data[1000:amount_of_rows].T
-Y_train = data_train[0]
-X_train = data_train[1:amount_of_features]
-
-X_train = X_train / 255.
-_, m_train = X_train.shape
+import os
+import imageio as iio
+from skimage import filters
+from skimage.color import rgb2gray  # only needed for incorrectly saved images
+from skimage.measure import regionprops
+import cv2
 
 
-def getImage():
-    data = asarray(Image.open('test.png'))
+def save_file():
+    # Checking if file is not empty
+    while os.stat("../number.txt").st_size == 0:
+        print("File is empty")
+        time.sleep(0.1)
 
+    f = open("../number.txt", "r")  # Reading file
+
+    # Saving the file into the array
+    arr = []
+    for i in range(0, 12544):
+        arr.append(int(f.read(1)))
+
+    arr = np.array(arr)
+    arr = arr*255
+
+    # Reshaping an array to a 2d array of pixels
+    arr2 = arr.reshape(112, 112)
+
+    # Convert the pixels into an array using numpy
+    array = np.array(arr2, dtype=np.uint8)
+
+    # Use PIL to create an image from the new array of pixels
+    new_image = Image.fromarray(array)
+    new_image.save('privateImage.png')
+
+    # Resizing the image to 28 by 28 for the AI
+    image = Image.open('privateImage.png')
+    new_image = image.resize((28, 28))
+    new_image.save('privateImage.png')
+
+    # To prevent black image problems
+    if sum(arr) == 0:
+        return 0
+    center_image()
+
+
+def center_image():
+    # Read image
+    img = cv2.imread("privateImage.png")
+
+    # finding center of mass of image
+    image = rgb2gray(iio.imread("privateImage.png"))
+    threshold_value = filters.threshold_otsu(image)
+    labeled_foreground = (image > threshold_value).astype(int)
+    properties = regionprops(labeled_foreground, image)
+    center_of_mass = properties[0].centroid
+
+    # size of the image
+    num_rows, num_cols = img.shape[:2]
+
+    # finding how much to move in each direction
+    move_x = center_of_mass[1] - num_rows/2
+    move_y = center_of_mass[0] - num_rows/2
+
+    # Creating a translation matrix
+    translation_matrix = np.float32([[1, 0, -move_x], [0, 1, -move_y]])
+
+    # Image translation
+    img_translation = cv2.warpAffine(img, translation_matrix, (num_cols + 0, num_rows + 0))
+
+    # Save image
+    cv2.imwrite("privateImage.png", img_translation)
+
+    # Remove the rbg type of image by converting it to grayscale & saving it
+    new_image = Image.fromarray(np.array(Image.open('privateImage.png').convert('L')))
+    new_image.save('privateImage.png')
+
+
+def get_image():
+    save_file()                   # Saving file in the proper shape needed
+
+    # Transferring the image into an array
+    data = asarray(Image.open('privateImage.png'))
     data = np.array(data)
     amount_of_rows, amount_of_features = data.shape
 
-    data_train = data
-    X_train = data_train[0:amount_of_features]
-    # print(amount_of_features)
-    X_train = X_train / 255
-    result = X_train.flatten()
+    X_image = data[0:amount_of_features]
+    X_image = X_image / 255                     # Getting a black & white image
+    result = X_image.flatten()                  # Transforming a 2d array into a 1d
 
-
+    # Creating an array of the pixels of the image
     arr = []
     for i in result:
         cr = [i]
@@ -44,31 +101,24 @@ def getImage():
     return arr
 
 
-def init_params():
-    W1 = np.random.rand(10, 784) - 0.5
-    b1 = np.random.rand(10, 1) - 0.5
-    W2 = np.random.rand(10, 10) - 0.5
-    b2 = np.random.rand(10, 1) - 0.5
-
-    # To initiate with current parameters
-    '''
-    w1_test = np.loadtxt("W1.txt").reshape(10, 784)
-    w2_test = np.loadtxt("W2.txt").reshape(10, 10)
-    b1_test = np.loadtxt("b1.txt").reshape(10, 1)
-    b2_test = np.loadtxt("b2.txt").reshape(10, 1)
-    '''
-    return W1, b1, W2, b2
-
-
+# ReLU is an activation function being used by the AI to keep the values positive and effective
 def ReLU(Z):
     return np.maximum(Z, 0)
 
 
+# softmax is an activation function being used by the AI to normalize values to a range between 0 -> 1
 def softmax(Z):
     A = np.exp(Z) / sum(np.exp(Z))
     return A
 
 
+# get_prediction returns the index of the maximum value in the array by axis=0
+def get_prediction(A2):
+    return np.argmax(A2, 0)
+
+
+# forward_prop using the activation functions and math functions like dot to calculate the output layer
+# by doing a forwards propagation on the layers of the AI
 def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
     A1 = ReLU(Z1)
@@ -77,196 +127,56 @@ def forward_prop(W1, b1, W2, b2, X):
     return Z1, A1, Z2, A2
 
 
-def ReLU_deriv(Z):
-    return Z > 0
-
-
-def one_hot(Y):
-    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-    one_hot_Y[np.arange(Y.size), Y] = 1
-    one_hot_Y = one_hot_Y.T
-    return one_hot_Y
-
-
-def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
-    one_hot_Y = one_hot(Y)
-    dZ2 = A2 - one_hot_Y
-    dW2 = 1 / amount_of_rows * dZ2.dot(A1.T)
-    db2 = 1 / amount_of_rows * np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
-    dW1 = 1 / amount_of_rows * dZ1.dot(X.T)
-    db1 = 1 / amount_of_rows * np.sum(dZ1)
-    return dW1, db1, dW2, db2
-
-
-def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, learning_rate):
-    W1 = W1 - learning_rate * dW1
-    b1 = b1 - learning_rate * db1
-    W2 = W2 - learning_rate * dW2
-    b2 = b2 - learning_rate * db2
-    return W1, b1, W2, b2
-
-
-def get_predictions(A2):
-    return np.argmax(A2, 0)
-
-
-def get_accuracy(predictions, Y):
-    print(predictions, Y)
-    return np.sum(predictions == Y) / Y.size
-
-
-def gradient_descent(X, Y, learning_rate, iterations):
-    W1, b1, W2, b2 = init_params()
-    for i in range(iterations):
-        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, learning_rate)
-        if i % 10 == 0:
-            print("Iteration: ", i)
-            predictions = get_predictions(A2)
-            print(get_accuracy(predictions, Y))
-    return W1, b1, W2, b2
-
-
-learning_rate = 0.20
-iterations = 1000
-# W1, b1, W2, b2 = gradient_descent(X_train, Y_train, learning_rate, iterations)
-# module accuracy 94.5%
-
-
+# make_predictions sends all necessary values to the forward propagation method
 def make_predictions(X, W1, b1, W2, b2):
     _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
-    print(A2)
-    predictions = get_predictions(A2)
+    # print(A2)                                 # -> To print the chances of each number in percentage
+    predictions = get_prediction(A2)
     return predictions
 
 
-def test_prediction(index, W1, b1, W2, b2):
-    current_image = X_train[:, index, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
-    label = Y_train[index]
-
-    print("Prediction: ", prediction)
-    print("Label: ", label)
-
-    current_image = current_image.reshape((28, 28)) * 255
-    plt.gray()
-    plt.imshow(current_image, interpolation='nearest')
-    plt.show()
-
-
-def test_prediction2(index, W1, b1, W2, b2):
+def send_prediction(index, W1, b1, W2, b2):
     current_image = index
-    print(current_image)
     current_image = np.array(current_image).T
     prediction = make_predictions(index, W1, b1, W2, b2)
-    label = "test"
 
-    print("Prediction: ", prediction)
-    print("Label: ", label)
+    # print("Label: ", "Image")
+    # print("Prediction: ", prediction)         # -> To print prediction on current image
 
-    # print("------------")
-    # print(current_image)
+    # Sends prediction to assembly by specific file
+    f = open("../result.txt", "w")
+    f.write(str(prediction[0]))
+    f.close()
 
+    # Display image on screen
+    '''
     current_image = current_image.reshape((28, 28)) * 255
-    print("------------")
-    # print(current_image)
+    # Showing image on screen
     plt.gray()
     plt.imshow(current_image, interpolation='nearest')
     plt.show()
+    '''
 
 
-def test_prediction3(index, W1, b1, W2, b2):
-    current_image = X_train[:, index, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
-    label = Y_train[index]
-
-    print("Prediction: ", prediction)
-    print("Label: ", label)
-
-    if prediction[0] == label:
-        return 0
-    else:
-        print("--------------Error--------------")
-        return 1
-
-
+# Loading AI data from saved files
 w1_test = np.loadtxt("W1.txt").reshape(10, 784)
 w2_test = np.loadtxt("W2.txt").reshape(10, 10)
 b1_test = np.loadtxt("b1.txt").reshape(10, 1)
 b2_test = np.loadtxt("b2.txt").reshape(10, 1)
 
 
-# print(two)
-# test_prediction2(two, w1_test, b1_test, w2_test, b1_test)
+while True:
+    print("Processing data...")
+    image = get_image()                         # Getting the image
+    # Testing image with AI + Printing prediction and changes
+    send_prediction(image, w1_test, b1_test, w2_test, b1_test)
 
-test = getImage()
-test_prediction2(test, w1_test, b1_test, w2_test, b1_test)
+    # Clearing number file
+    file = open("../number.txt", "r+")
+    file.truncate(0)
+    file.close()
 
-'''
-for i in range(0):
-    # test_prediction(i, W1, b1, W2, b2)
-    test_prediction(i, w1_test, b1_test, w2_test, b2_test)
-    i += 1
-'''
-
-# num_of_error = 0
-# for i in range(41000):
-#
-#     # test_prediction(i, W1, b1, W2, b2)
-#     x = test_prediction3(i, w1_test, b1_test, w2_test, b2_test)
-#     num_of_error += x
-#     i += 1
-# print(num_of_error)
-
-'''
-a_file = open("W1.txt", "w")
-for row in W1:
-    np.savetxt(a_file, row)
-a_file.close()
-
-original_array = np.loadtxt("W1.txt").reshape(10, 784)
-print("----")
-print(original_array)
+    time.sleep(0.1)
 
 
-a_file = open("W2.txt", "w")
-for row in W2:
-    np.savetxt(a_file, row)
-a_file.close()
 
-original_array = np.loadtxt("W2.txt").reshape(10, 10)
-print("----")
-print(original_array)
-
-
-a_file = open("b1.txt", "w")
-for row in b1:
-    np.savetxt(a_file, row)
-a_file.close()
-
-original_array = np.loadtxt("b1.txt").reshape(10, 1)
-print("----")
-print(original_array)
-
-a_file = open("b2.txt", "w")
-for row in b1:
-    np.savetxt(a_file, row)
-a_file.close()
-
-original_array = np.loadtxt("b2.txt").reshape(10, 1)
-print("----")
-print(original_array)
-'''
-
-'''
-print("-----")
-print(W1)
-print("-----")
-print(W2)
-print("-----")
-print(b1)
-print("-----")
-print(b2)
-'''
